41d4d1724e388fa23fd2947c7ef69f96b13e1a47
Daniel Uribe <duribegalaz@gmail.com>
2023-04-18 00:47:48 -0400

    Consumo de API

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # API-Digimon-practica
+
+Hola profesor :)
+
+Le dejo mi repositorio que está en la ruta:
+
+    https://github.com/Duridev/API-Digimon-practica
+
+Tambien estátengo mi Github Page:
+
+    https://duridev.github.io/API-Digimon-practica/
+
+Quedo al pendiente de cualquier comentario.
+
+Muchas Gracias!!!! :D
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,31 @@
 const URL_BASE = 'https://digimon-api.vercel.app/';
-let contenido = document.getElementById("contenido");
-
+const URL_DIGIMONS = URL_BASE+'/api/digimon/name/'
+let contenido;
 
 function tabla(datos) {
     contenido.innerHTML = "";
 
-for (let temp of datos) {
-    contenido.innerHTML +=
-        `<tr>
-            <th scope="row">${datos.lenght}</th>
-            <td>${temp.name}</td>
-            <td>${temp.img}</td>
-            <td>${temp.level}</td>
-        </tr>`;
+    for (let temp of datos) {
+        contenido.innerHTML +=`<tr>
+                <td>${temp.name}</td>
+                <td><img width="80rem" src="${temp.img}"></td>
+                <td>${temp.level}</td>
+            </tr>`;
     }
 }
 
-fetch(URL_BASE+'api/digimon')
+digibuscar() {
+
+}
+
+$(document).ready(function (){
+    contenido = document.getElementById("contenido");
+
+    fetch(URL_BASE+'api/digimon')
     .then(response => response.json())
     .then(datos => {
-        console.log(datos);
+        console.log(datos)
         tabla(datos);
-    })
+    });
+})
+
